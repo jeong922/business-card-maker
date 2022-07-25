50378c2a1fb9b78835255d1481cb08df4b0fe534
jeong922 <hwajeong922@naver.com>
2022-07-25 14:32:05 +0900

    로그인 기능 추가

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -3,8 +3,11 @@ import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
