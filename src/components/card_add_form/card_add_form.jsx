fff012f256a2f1285608d32a9e30d7a54bfd5606
jeong922 <hwajeong922@naver.com>
2022-07-26 17:26:19 +0900

    CardAddForm 디자인 변경

--- src/components/card_add_form/card_add_form.jsx
+++ src/components/card_add_form/card_add_form.jsx
@@ -4,7 +4,7 @@ import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_add_form.module.css';
 import { v4 as uuidv4 } from 'uuid';
 
-const CardAddForm = ({ onAdd }) => {
+const CardAddForm = ({ onAdd, setNewCard }) => {
   const formRef = useRef();
   const nameRef = useRef();
   const companyRef = useRef();
@@ -27,59 +27,70 @@ const CardAddForm = ({ onAdd }) => {
     };
     formRef.current.reset();
     onAdd(card);
+    setNewCard(false);
   };
-  return (
-    <form ref={formRef} className={styles.form}>
-      <input
-        ref={nameRef}
-        className={styles.input}
-        type="text"
-        name="name"
-        placeholder="Name"
-      />
-      <input
-        ref={companyRef}
-        className={styles.input}
-        type="text"
-        name="company"
-        placeholder="Company"
-      />
-      <select
-        ref={themeRef}
-        className={styles.select}
-        name="theme"
-        placeholder="Theme"
-      >
-        <option placeholder="light">light</option>
-        <option placeholder="dark">dark</option>
-        <option placeholder="colorful">colorful</option>
-      </select>
-      <input
-        ref={titleRef}
-        className={styles.input}
-        type="text"
-        name="title"
-        placeholder="Title"
-      />
-      <input
-        ref={emailRef}
-        className={styles.input}
-        type="text"
-        name="email"
-        placeholder="Email"
-      />
-      <textarea
-        ref={messageRef}
-        className={styles.textarea}
-        name="message"
-        placeholder="Message"
-      ></textarea>
-      <div className={styles.fileInput}>
-        <ImageFileInput />
-      </div>
 
-      <Button name="Add" onClick={onSubmit} />
-    </form>
+  const onClose = (e) => {
+    if (e.target === e.currentTarget) {
+      setNewCard(false);
+    }
+  };
+
+  return (
+    <div onClick={onClose} className={styles.overlay}>
+      <div className={styles.container}>
+        <form ref={formRef} className={styles.form}>
+          <input
+            ref={nameRef}
+            className={styles.input}
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+          <input
+            ref={companyRef}
+            className={styles.input}
+            type="text"
+            name="company"
+            placeholder="Company"
+          />
+          <select
+            ref={themeRef}
+            className={styles.select}
+            name="theme"
+            placeholder="Theme"
+          >
+            <option placeholder="light">light</option>
+            <option placeholder="dark">dark</option>
+            <option placeholder="colorful">colorful</option>
+          </select>
+          <input
+            ref={titleRef}
+            className={styles.input}
+            type="text"
+            name="title"
+            placeholder="Title"
+          />
+          <input
+            ref={emailRef}
+            className={styles.input}
+            type="text"
+            name="email"
+            placeholder="Email"
+          />
+          <textarea
+            ref={messageRef}
+            className={styles.textarea}
+            name="message"
+            placeholder="Message"
+          ></textarea>
+          <div className={styles.fileInput}>
+            <ImageFileInput />
+          </div>
+          <Button name="Add" onClick={onSubmit} />
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from '../button/button';
 import CardAddForm from '../card_add_form/card_add_form';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
 const Editor = ({ cards, addCard, updateCard, deleteCard }) => {
+  const [newCard, setNewCard] = useState(false);
+
+  const onClick = () => {
+    setNewCard(true);
+  };
+
+  useEffect(() => {
+    if (newCard) {
+      document.body.style.cssText = `
+      overflow-y: hidden;`;
+    }
+    return () => {
+      document.body.style.cssText = `
+      overflow-y: 'auto';
+      `;
+    };
+  }, [newCard]);
+
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
+      <div className={styles.button}>
+        <Button name="New Card" onClick={onClick} />
+      </div>
       {Object.keys(cards).map((key) => (
         <CardEditForm
           key={key}
@@ -15,7 +37,7 @@ const Editor = ({ cards, addCard, updateCard, deleteCard }) => {
           deleteCard={deleteCard}
         />
       ))}
-      <CardAddForm onAdd={addCard} />
+      {newCard && <CardAddForm onAdd={addCard} setNewCard={setNewCard} />}
     </section>
   );
 };
