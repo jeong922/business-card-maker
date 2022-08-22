2e2e38e97b470c341c145e08487b8624081e5456
jeong922 <hwajeong922@naver.com>
2022-08-22 20:37:19 +0900

    깃허브 배포 오류 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,28 @@
-import { useCallback } from "react";
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styles from "./app.module.css";
-import Login from "./components/login/login";
-import Maker from "./components/maker/maker";
-import Todos from "./components/todos/todos";
+import { useCallback } from 'react';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styles from './app.module.css';
+import Login from './components/login/login';
+import Maker from './components/maker/maker';
+import Todos from './components/todos/todos';
 
 function App({ FileInput, authService, cardRepository, todoRepository }) {
   const getTheme = useCallback(() => {
-    const theme = localStorage.getItem("mytheme");
+    const theme = localStorage.getItem('mytheme');
     if (!theme) {
-      return "light";
+      return 'light';
     } else {
       return theme;
     }
   }, []);
 
-  const [isDark, setIsDark] = useState(getTheme); // true : dark , false : light
+  const [isDark, setIsDark] = useState(getTheme);
 
-  const theme = isDark === "dark" ? styles.dark : styles.light;
+  const theme = isDark === 'dark' ? styles.dark : styles.light;
 
   return (
     <div className={`${styles.app} ${theme}`}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route
             path="/"
